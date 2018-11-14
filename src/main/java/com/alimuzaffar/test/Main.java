efdacfbc060a154d5af6e7271c1c6e7bf7699de6
Ali Muzaffar <ali.muzaffar@bcgdv.com>
2018-11-14 14:15:57 +1100

    Testing printing of a Xmas tree

--- src/main/java/com/alimuzaffar/test/Main.java
+++ src/main/java/com/alimuzaffar/test/Main.java
@@ -1,22 +1,45 @@
 package com.alimuzaffar.test;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
-    public static void main(String [] args) {
-        String[] wordsList = {"Car", "Racoon"};
-        String searchInput = "c r";
 
-        String searchRegEx = searchInput.replace(" ", ".{1}");
-        Pattern pattern = Pattern.compile(searchRegEx, Pattern.CASE_INSENSITIVE);
-        System.out.println("RegEx (case-insensitive) is: " + searchRegEx);
+    public static void main(String[] args) {
+        final int treeHeight = 10;
+        final int trunkHeight = treeHeight - ((treeHeight * 4) / 5); // This will round down, round up instead?
+        final int leafHeight = treeHeight - trunkHeight;
 
-        for (String word : wordsList){
-            Matcher matcher = pattern.matcher(word);
-            boolean match = matcher.matches();
-            System.out.println("Test word '"+word + "' and match was: " + match);
+        final int spaceForNum = String.valueOf(treeHeight).length();
+        final String numFormat = "%"+spaceForNum+"d|";
+        final int maxStars = (leafHeight * 2) - 1;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = treeHeight; i > 0; i--) {
+            sb.append(String.format(numFormat, i));
+            int stars = (treeHeight - i) == 0? 1 : (treeHeight - i)*2 + 1;
+            int space = (maxStars - stars) != 0? (maxStars - stars)/2 : 0;
+            if (space < 0) {
+                stars = 3;
+                space = (maxStars - stars)/2;
+            }
+            String starsStr = repeatChar(stars, '*');
+            if (space != 0 && space > 0) {
+                String spaceStr = repeatChar(space, ' ');
+                sb.append(spaceStr);
+                sb.append(starsStr);
+                sb.append(spaceStr);
+            } else if (space == 0){
+                sb.append(starsStr);
+            }
+            sb.append(String.format("|%d=%d+%d+%d%n", maxStars, space, stars, space));
         }
+        System.out.println(sb.toString());
+    }
+
+    static String repeatChar(int count, char character) {
+        StringBuilder sb = new StringBuilder(count);
+        for (int i = 0; i < count; i++) {
+            sb.append(character);
+        }
+        return sb.toString();
     }
 }
